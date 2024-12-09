892b45ede03e009d894f0a5d2e30a5a0d1cbbbc7
Muhammad Alhamdi <hamdi1445@gmail.com>
2024-12-09 19:34:23 +0700

    book detail view

--- resources/views/bookdetail.blade.php
+++ resources/views/bookdetail.blade.php
@@ -5,8 +5,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>BookDetail</title>
+    <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('stylesbookdetail.css') }}">
 </head>
 <body>
-
+    <div class="imgcontainer">
+        <img src="" alt="">
+    </div>
 </body>
 </html>

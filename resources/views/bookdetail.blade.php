3693f96c1481a7305f88da4824f255f7ef3d8bd7
Muhammad Alhamdi <hamdi1445@gmail.com>
2024-12-09 23:28:05 +0700

    fix udah

--- resources/views/bookdetail.blade.php
+++ resources/views/bookdetail.blade.php
@@ -1,15 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>BookDetail</title>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <link rel="stylesheet" href="{{ asset('stylesbookdetail.css') }}">
-    <link href='https://fonts.googleapis.com/css?family=Urbanist' rel='stylesheet'>
-</head>
-<body>
+@extends('admin')
+@section('content')
     <div class="book-detail">
         <div class="book-image">
             <img src="{{ asset($book->Image) }}" alt="{{ $book->Title }}">
@@ -68,5 +58,4 @@
             </div>
         </div>
     </div>
-</body>
-</html>
+    @endsection

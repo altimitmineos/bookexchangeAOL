0b6d8ad923edaf9413b338a995b143fb99e10acb
Muhammad Alhamdi <hamdi1445@gmail.com>
2024-12-17 03:42:26 +0700

    added fixed create book

--- resources/views/guest/categorypageguest.blade.php
+++ resources/views/guest/categorypageguest.blade.php
@@ -25,7 +25,7 @@
             <div class="col-md-2">
                 <a href="{{ route('bookdetail-guest', $book->id) }}" class="text-decoration-none">
                     <div class="card">
-                        <img src="{{ asset($book->Image) }}" class="card-img-top" alt="{{ $book->Title }}">
+                        <img src="{{ asset('images/' . $book->Image) }}" class="card-img-top" alt="{{ $book->Title }}">
                         <div class="card-body">
                             <h5 class="card-title">{{ $book->Title }}</h5>
                             <p class="card-text">{{ $book->Author }}</p>
